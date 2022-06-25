714bf7b5599e60db4258f169f207e9d44680deb4
davidhewitt <davidhewitt@users.noreply.github.com>
2022-06-25 20:43:15 +0000

    Upload documentation for main

---
+++ main/debug/deps/smallvec-722e359f63a47073.d
@@ -0,0 +1,5 @@
+/home/runner/work/pyo3/pyo3/target/guide/debug/deps/smallvec-722e359f63a47073.rmeta: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-1.8.1/src/lib.rs
+
+/home/runner/work/pyo3/pyo3/target/guide/debug/deps/smallvec-722e359f63a47073.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-1.8.1/src/lib.rs
+
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-1.8.1/src/lib.rs:
